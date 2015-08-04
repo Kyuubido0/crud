21dc749fb87f846de09b2d9b2cc83f2e7cdf7085
Kyuubido0 <andreea_laura_cristescu@yahoo.com>
2015-08-04 12:22:40 +0300

    Edit form and added inactive

--- CRUD.js
+++ CRUD.js
@@ -2,32 +2,32 @@ $(document).ready(function() {
 	
 	/* Main Function - Delete, Add, Hide, Show */
 	
+	var contentCancel = "";
     $('.options').hide();
-	
+
     $('#single-answer').on('click', function() {
         $('.options').hide();
     });
-    
+    var choiceNumber = 2;
+    var ansNR = 2;
     $('#multiple-answer').on('click', function() {
-		
         $('.options').show();
-        var choiceNumber = 2;
-		
+		choiceNumber = 2;
         $('.add-choice').on('click', function(e) {
 			
-        e.preventDefault();
-        choiceNumber++;
-        var input = "";
-			input += "<li class='list'>Choice #" + choiceNumber + "<input type='text' name='choice' id='choice'>";
-			input += "<input type='image' src='cross108.png' class='icon-del' alt='Submit'></li>";
-        $('.poll-list').append(input);
+	        e.preventDefault();
+	        choiceNumber++;
+	        var input = "";
+				input += "<li class='list'>Choice #" + choiceNumber + "<input type='text' name='choice' id='choice" + choiceNumber + "'>";
+				input += "<input type='image' src='cross108.png' class='icon-del' alt='Submit'></li>";
+	        $('.poll-list').append(input);
 		});	
 		
+		ansNR = choiceNumber;
 		$('ol').on('click', '.icon-del', function() {
         $(this).closest('.list').remove();
         choiceNumber -= 1;
         });	
-
 	});
 	
 	/*  Add answers */
@@ -35,77 +35,121 @@ $(document).ready(function() {
 	$('.button-add').on('click', function() {
 		
 		var output = "<div class='inactive-form'>";
-			output += "Question: <input type='text' name='quesion-answer' id='question-answer'>";
-			output += "<input type='image' src='pencil117.png' class='icon-edit' alt='Submit'>";
-			output += "<input type='image' src='cross108.png' class='icon-delete' alt='Submit'>";
-			output += "<p class='order-p'> Answer: </p>";
-			output += "</div>";
+			output += "<div class='controls'><input type='image' src='pencil117.png' class='icon-edit' alt='Submit'>";
+			output += "<input type='image' src='cross108.png' class='icon-delete' alt='Submit'></div>";
+			output += "<strong>Question:</strong> <div class='questionToAnswer' id='questionToAnswer' style='clear:none;'>" + document.getElementById('txtQuestion').value + "</div>";
+			output += "<p class='order-p'> <strong>Answer:</strong> <div id='txtAnswers'>";
+		
+			if(document.getElementById('single-answer').checked)
+			{
+				output += "Yes / No</div>";
+			}
+			else if(document.getElementById('multiple-answer').checked)
+			{
+				for(var k = 1;k <= choiceNumber;k++)
+				{
+					output += "<div id='answer" + k + "'>" + document.getElementById('choice' + k).value + "</div>";
+				}
+				output += "</div>";
+			}
+			output += "</p></div>";
 		$('.add-poll').append(output);
-		
-		$('.inactive-form').on('click', '.icon-delete', function() {
-        $(this).closest('.inactive-form').remove();
-        });
-		
-		
-		$('.inactive-form').on('click', '.icon-edit', function(e) {
-
-		e.preventDefault();
-		//console.log('merge');
-		var edit = "<div class='add-edit'>";;
-            edit += "<strong>Question:</strong>";
-            edit += "<input type='text' name='question'>";
-			edit += "<br>";
-            edit += "<label for='single-answer-edit'>";
-            edit += "<strong>Answers:</strong>";
-            edit += "<input type='radio' name='answer' id='single-answer-edit' class='radio-buttons' value='yes/no'>Yes/No form";
-            edit += "</label>";
-            edit += "<label for='multiple-answer-edit'>";
-            edit += "<input type='radio' name='answer' id='multiple-answer-edit' class='radio-buttons' value='multiple'>Multiple choice form";
-            edit += "</label>"; 
-			edit += "<div class='options-edit'>";
-            edit += "<ol class='poll-list-edit'>";
-            edit += "<li class='list-edit'>Choice #1<input type='text' name='choice-edit'></li>";
-            edit += "<li class='list-edit'>Choice #2<input type='text' name='choice' id='choice-edit'></li>"
-            edit += "</ol>";
-            edit += "<a href='#' class='add-choice-edit'>+ Add new choice </a>";	
-            edit += "</div>";
-            edit += "<div class=''></div>";
-            edit += "<button type='button' name='buttonPoll' id='buttonPollEdit' class='button-edit'>Edit Poll</button>";
-			edit += "<a href='#' class='cancel-edit'> Cancel </a>";
-            edit +=	"</div>";
-		$('.add-edit').append(output);
-			
-			
-			/* Multiple Choices */
-			
-		$('.options-edit').hide();
-	
-    	$('#single-answer-edit').on('click', function() {
-        $('.options').hide();
-    	});
-    
-    	$('#multiple-answer-edit').on('click', function() {
-		
-        $('.options-edit').show();
-        var choiceNumberEdit= 2;
-		
-        $('.add-choice-edit').on('click', function(e) {
-			
-        e.preventDefault();
-        choiceNumberEdit++;
-        var input = "";
-			input += "<li class='list-edit'>Choice #" + choiceNumber + "<input type='text' name='choice' id='choice-edit'>";
-			input += "<input type='image' src='cross108.png' class='icon-delete-edit' alt='Submit'></li>";
-        $('.poll-list').append(input);
-		});	
-			
-		$('ol').on('click', '.icon-delete-edit', function() {
-        $(this).closest('.list-edit').remove();
-        choiceNumberEdit -= 1;
-        });
-		
-			});
-		});
 	});
+
+	$('.wrapper').on('click', '.icon-delete', function() {
+        $(this).closest('.inactive-form').remove();
+    });
 	
+	$('.wrapper').on('click', '.icon-edit', function() {
+
+		contentCancel = $(this).closest('.inactive-form').html();
+		var edit = "";
+			edit += '<input type="text" name="questionEdit" id="txtQuestionEdit" value="'+ $(this).closest('.inactive-form').find('#questionToAnswer').text() +'">';
+		var multi = "";
+		var ans = "";
+			ans += '<label for="editsingle-answer">';
+			ans += '<input type="radio" name="answer" id="editsingle-answer" class="radio-buttons" value="yes/no">Yes/No form';
+			ans += '</label>';
+			ans += '<br>';
+			ans += '<label for="editmultiple-answer">';
+			ans += '<input type="radio" name="answer" id="editmultiple-answer" class="radio-buttons" value="multiple">Multiple choice form';
+			ans += '</label>';
+			ans += '<div class="editoptions" style="display:none;">';
+			ans += '<ol class="editpoll-list">';
+
+			if($(this).closest('.inactive-form').find('#txtAnswers').text()!="Yes / No")
+			{
+				for(var k = 1;k <= choiceNumber; k++)
+				{
+					multi += '<input type="text" id="editAns' + k + '" value="'+ $(this).closest('.inactive-form').find('#answer'+k).text() +'"><br />';
+				}
+			}
+
+			if(multi == "")
+			{
+				ans += '<input type="text" id="editAns1"> <br/>';
+				ans += '<input type="text" id="editAns2"> <br/>';
+			}
+			else
+			{
+				ans += multi;
+			}
+			ans += '</ol>';
+			ans += '<a href="#" class="editadd-choice"> + Add new choice</a>';
+			ans += '</div>';
+
+		$(this).closest('.inactive-form').find('#questionToAnswer').html(edit);
+
+		$(this).closest('.inactive-form').find('#txtAnswers').html(ans);
+
+		$('#editmultiple-answer').on('click', function() {
+       		$('.editoptions').show();
+       	});
+
+       	$('#editsingle-answer').on('click', function() {
+       		$('.editoptions').hide();
+       	});
+       	var ansNumb = 2;
+       	$('.editadd-choice').on('click', function(e) {
+			
+	        e.preventDefault();
+	        ansNumb++;
+	        var input = "";
+				input +='<input type="text" id="editAns'+ ansNumb +'"><br />';
+	        $('.editpoll-list').append(input);
+		});
+
+		$(this).closest('.inactive-form').append('<button type="button" id="editPoll" class="button-add">Edit Poll</button> <a href="#" id="btnCancel" style="margin-left: 20px">Cancel</a>');
+		
+		$('#editPoll').on('click', function() {
+			var output = "<div class='inactive-form'>";
+				output += "<div class='controls'><input type='image' src='pencil117.png' class='icon-edit' alt='Submit'>";
+				output += "<input type='image' src='cross108.png' class='icon-delete' alt='Submit'></div>";
+				output += "Question: <div class='questionToAnswer' id='questionToAnswer' style='clear:none;'>" + document.getElementById('txtQuestionEdit').value + "</div>";
+				output += "<p class='order-p'> Answer: <div id='txtAnswers'>";
+				if(document.getElementById('editsingle-answer').checked)
+				{
+					output += "Yes / No</div>";
+				}
+				else if(document.getElementById('editmultiple-answer').checked)
+				{
+					for(var k = 1;k <= ansNumb;k ++)
+					{
+						output += "<div id='answer" + k + "'>" + document.getElementById('editAns' + k).value + "</div>";
+					}
+					output +="</div>";
+				}
+				output += "</p></div>";
+			$('.add-poll').append(output);
+			$(this).closest('.inactive-form').remove();
+		});
+
+		$('#btnCancel').on('click', function() {
+			$(this).closest('.inactive-form').remove();
+			var output = "<div class='inactive-form'>";
+				output += contentCancel;
+				output += "</div>";
+			$('.add-poll').append(output);
+		});		
+	});	
 });
